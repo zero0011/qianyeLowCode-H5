fb3f188befbde25c1ab34e26329247e715242397
zsshuaizhu <2732237782@qq.com>
2024-03-30 18:51:55 +0800

    feat: reload

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,14 +4,22 @@ import UserHeadBtn from "@/components/UserHeadBtn";
 
 const { Header } = Layout
 
-const RootHeader = () => (
-  <Header className="header clearfix fixed center" >
-    <div className="logo">qianyeLowCode-H5</div>
-    <div className="header-center-wrapper"></div>
-    <div className="user-wrapper">
-      <UserHeadBtn />
-    </div>
-  </Header>
-)
+const RootHeader: React.FC = () => {
+
+  const goHome = () => {
+    // 刷新网站
+    window.location.reload()
+  }
+
+  return (
+    <Header className="header clearfix fixed center" >
+      <div className="logo" onClick={goHome}>qianyeLowCode-H5</div>
+      <div className="header-center-wrapper"></div>
+      <div className="user-wrapper">
+        <UserHeadBtn />
+      </div>
+    </Header>
+  )
+}
 
 export default RootHeader;
