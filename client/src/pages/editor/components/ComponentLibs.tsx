cb27863c51bb2af7b631aede06844d21f1854818
zsshuaizhu <2732237782@qq.com>
2024-03-31 22:21:50 +0800

    feat: 引入滚动组件

--- client/src/pages/editor/components/ComponentLibs.tsx
+++ client/src/pages/editor/components/ComponentLibs.tsx
@@ -1,8 +1,25 @@
 import React, { FC } from "react";
+import eleConfig from "@/config/eleConfig";
 
 const ComponentLibs: FC = () => {
+
+
+
   return (
-    <div>ComponentLibs</div>
+    <div className="components-libs-wrapper scrollbar-wrapper">
+      <p className="page-title text-center">组件库</p>
+      <div style={{ height: '100px' }}>
+        <ul className="scrollbar-wrapper">
+          {eleConfig
+            .map(ele =>
+              <li className="clearfix paddingB30">
+                <div className="components-libs-title">
+                  <p>{ele.title}</p>
+                </div>
+              </li>)}
+        </ul>
+      </div>
+    </div>
   )
 }
 
