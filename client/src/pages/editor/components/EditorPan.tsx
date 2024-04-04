9e3a32266ab9180df88db00cef2ffe93bc3b59ed
zsshuaizhu <2732237782@qq.com>
2024-04-04 18:50:06 +0800

    feat: 交互事件

--- client/src/pages/editor/components/EditorPan.tsx
+++ client/src/pages/editor/components/EditorPan.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { FC } from "react";
 
-function EditorPan () {
+interface EditorPanType {
+
+}
+
+const editorMainStyle = {
+  'transform': 'scale(1)',
+  'width': '375px',
+  'height': '644px'
+}
+
+const EditorPan: FC<EditorPanType> = () => {
   return (
     <div className="editor-pane">
-      
+      <div className="editor-pane-inner">
+        <div className="editor-main" style={editorMainStyle}>
+          <div className="page-preview-wrapper"></div>
+
+          <div className="page-wrapper-mask"></div>
+        </div>
+      </div>
     </div>
   )
 }
